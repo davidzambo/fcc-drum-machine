ee69a61e629e50f3de610f234debd48888d6aa81
David Zambo <david@dcmf.hu>
2018-06-17 10:10:41 +0200

    Wav to mp3 convert

--- src/App.js
+++ src/App.js
@@ -15,15 +15,15 @@ class App extends Component {
                     <h1>freeCodeCamp Drum Machine</h1>
                 </header>
                 <main className="drum-machine">
-                    <Sample audio="audio/tom-left.wav" image="images/left-tom.png" title="Tom 01" trigger="q"/>
-                    <Sample audio="audio/tom-right.wav" image="images/right-tom.png" title="Tom 02" trigger="w"/>
-                    <Sample audio="audio/tom-stand.wav" image="images/stand-tom.png" title="Tom 03" trigger="e"/>
-                    <Sample audio="audio/closed-hihat.wav" image="images/closed-hihat.png" title="Closed Hi-hat" trigger="a"/>
-                    <Sample audio="audio/snare.wav" image="images/snare.png" title="Snare" trigger="s"/>
-                    <Sample audio="audio/crash.wav" image="images/crash.png" title="Crash Cymbals" trigger="d"/>
-                    <Sample audio="audio/half-hihat.wav" image="images/half-hihat.png" title="Opened Hi-hat" trigger="z"/>
-                    <Sample audio="audio/kick.wav" image="images/kick.png" title="Kick" trigger="x"/>
-                    <Sample audio="audio/snare-rim.wav" image="images/snare-rim.png" title="Snare Rim" trigger="c"/>
+                    <Sample audio="audio/tom-left.mp3" image="images/left-tom.png" title="Tom 01" trigger="q"/>
+                    <Sample audio="audio/tom-right.mp3" image="images/right-tom.png" title="Tom 02" trigger="w"/>
+                    <Sample audio="audio/tom-stand.mp3" image="images/stand-tom.png" title="Tom 03" trigger="e"/>
+                    <Sample audio="audio/closed-hihat.mp3" image="images/closed-hihat.png" title="Closed Hi-hat" trigger="a"/>
+                    <Sample audio="audio/snare.mp3" image="images/snare.png" title="Snare" trigger="s"/>
+                    <Sample audio="audio/crash.mp3" image="images/crash.png" title="Crash Cymbals" trigger="d"/>
+                    <Sample audio="audio/half-hihat.mp3" image="images/half-hihat.png" title="Opened Hi-hat" trigger="z"/>
+                    <Sample audio="audio/kick.mp3" image="images/kick.png" title="Kick" trigger="x"/>
+                    <Sample audio="audio/snare-rim.mp3" image="images/snare-rim.png" title="Snare Rim" trigger="c"/>
                 </main>
                 <footer>
                     <a href="https://www.dcmf.hu" target="_blank" rel="noopener noreferrer">
